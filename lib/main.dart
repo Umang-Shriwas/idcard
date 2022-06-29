0f6c82c325afae4337b25f4befd9ca21ee53f05d
Umang-Shriwas <umangshriwas17@gmail.com>
2022-06-29 14:18:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:idcard/model/card_model.dart';  
+  
+void main() => runApp(MyApp());  
+  
+/// This Widget is the main application widget.  
+class MyApp extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
+    return MaterialApp(  
+      home: Scaffold(  
+        appBar: AppBar(title: Text('Flutter Card Example')),  
+        backgroundColor: Colors.white,  
+        body: MyCardWidget(),  
+      ),  
+    );  
+  }  
+}  
+  
+/// This is the stateless widget that the main application instantiates.  
+class MyCardWidget extends StatelessWidget {  
+  MyCardWidget({Key? key}) : super(key: key);  
+  
+  @override  
+  Widget build(BuildContext context) {  
+    return Center(  
+      widthFactor: 500,
+      heightFactor: 300,
+      
+      child: ListView.builder(
+        
+        itemCount: datalist.length,
+        itemBuilder:(context, index) =>  Card( 
+           
+                shape: RoundedRectangleBorder(  
+                  borderRadius: BorderRadius.circular(15.0),  
+                ),  
+                color: Colors.cyan,  
+                elevation: 10,  
+                child: Column(  
+                  mainAxisSize: MainAxisSize.min,  
+                  children: <Widget>[  
+                    ListTile(                
+                      leading: CircleAvatar(                 
+                        child: Container(  
+                          decoration: BoxDecoration(
+                           borderRadius: BorderRadius.circular(100),
+                            image: DecorationImage(
+                            image: AssetImage(datalist[index].images), fit: BoxFit.cover,
+                            
+                              )
+                           ),
+                        ),
+                      ),  
+                      title:  Text(  
+                        datalist[index].name,  
+                        style: TextStyle(fontSize: 20.0),),
+                        subtitle: Row(
+                          children: [
+                            Text(  
+                            'email:-umangshriwas17@gmail.com', 
+                            style: TextStyle(fontSize: 16.0)  
+                             ),
+                             Text(  
+                            'contact:-9179577244', 
+                            style: TextStyle(fontSize: 16.0)  
+                             ),
+                             
+                             
+                          ],
+                        ),
+                    ), 
+                  ],  
+                ),  
+              ),)
+    );  
+  }  
+} 
---
+++ lib/model/card_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class MyData{
+  String name;
+  String images;
+ 
+  MyData(this.name,this.images);
+}
+
+List<MyData>datalist=[
+  MyData("umang","images/id.png"),
+  MyData("Pankaj","images/log.png"),
+  MyData("Suraj","images/register.png"),
+] ;
+
+
+
